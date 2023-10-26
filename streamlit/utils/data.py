3efab58ae5367b78070b3b23f951066c42c5b1c4
Juanchobanano <juancepeda.gestion@gmail.com>
2023-10-26 13:14:57 +0300

    Fix streamlit data paths

--- streamlit/utils/data.py
+++ streamlit/utils/data.py
@@ -4,11 +4,11 @@ import pandas as pd
 
 def get_populations_data() -> Dict:
     dataframes = {}
-    files = os.listdir("./data/gold/populations")
+    files = os.listdir("./streamlit/data/gold/populations")
     for file in files:
         district_name = file.split("_")[0]
         df = pd.DataFrame(
-            pd.read_csv(f'./data/gold/populations/{file}').rename(columns={'Unnamed: 0': 'Year'})
+            pd.read_csv(f'./streamlit/data/gold/populations/{file}').rename(columns={'Unnamed: 0': 'Year'})
         )
         df["Year"] = df["Year"].astype(str)
         dataframes[district_name] = df
@@ -22,9 +22,9 @@ def get_population_data() -> pd.DataFrame:
     return pd.concat(df)
 
 def get_total_population_data() -> pd.DataFrame:
-    df = pd.read_csv("./data/gold/total/total_population_per_district.csv", index_col=0)
+    df = pd.read_csv("./streamlit/data/gold/total/total_population_per_district.csv", index_col=0)
     return df
 
 def get_housing_predictions() -> pd.DataFrame:
-    df = pd.read_csv("./data/gold/dataset/test_dataset_pred.csv", index_col=0)
+    df = pd.read_csv("./streamlit/data/gold/dataset/test_dataset_pred.csv", index_col=0)
     return df
